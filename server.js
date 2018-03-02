b43bd0686c480d8426f0ca2360eae6490716a87f
Mario Flores Jr <m.flores4821@gmail.com>
2018-03-02 11:36:27 -0800

    form routing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bodyParser = require('body-parser').urlencoded({extended: true});
 
 const app = express();
 const PORT = process.env.PORT;
@@ -19,27 +20,25 @@ app.get('/api/v1/books', (req,res) => {
     .then(result => res.send(result.rows));
 });
 
-app.get('api/v1/books/:id', (req,res) => {
-  console.log('helloooo');
+app.get('/api/v1/books/:id', (req,res) => {
   client.query(`SELECT * FROM books WHERE book_id=${req.params.id};`)
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
 
-app.post('/api/v1/books', (req,res) => {
+app.post('/api/v1/books', bodyParser, (req,res) => {
   client.query(
-    'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;',
+    'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);',
     [
       req.body.title,
       req.body.author,
       req.body.isbn,
       req.body.image_url,
       req.body.description
-    ],
-    function(err) {
-      if(err) console.err(err);
-    }
-  );
+    ]
+  )
+    .then(res.sendStatus(201))
+    .catch(console.error);
 });
 
 app.listen(PORT, () => console.log(`Listening on port:  ${PORT}`));
